d0649a2be04247a34e2e124ec0d3c9e8629bd897
bbimber <bbimber@gmail.com>
2016-04-13 10:01:42 -0700

    Initial refactor of ccc_import tool

---
+++ setup.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+with open('README.md') as readme_file:
+    readme = readme_file.read()
+
+requirements = [
+    "docopt>=0.6.2",
+    "elasticsearch>=2.3.0",
+    "python-dateutil>=2.5.0",
+    "pytz>=2015.7",
+    "requests>=2.9.1",
+    "six>=1.10.0",
+    "tsv>=1.1",
+    "urllib3>=1.14"
+]
+
+test_requirements = [
+    # TODO: put package test requirements here
+]
+
+setup(
+    name='ccc_import',
+    version='0.1.0',
+    description="Command line tool for importing/registering resources with CCC.",
+    long_description=readme,
+    author="Ben Bimber",
+    author_email='bimber@ohsu.edu',
+    url='https://github.com/ohsu-computational-biology/ccc_import',
+    packages=[
+        'ccc_import',
+    ],
+    package_dir={'ccc_import':
+                 'ccc_import'},
+    include_package_data=True,
+    install_requires=requirements,
+    license="BSD",
+    zip_safe=False,
+    keywords='ccc_import',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+    ],
+    scripts=['bin/ccc_import'],
+    test_suite='test',
+    tests_require=test_requirements,
+    dependency_links = [
+        #TODO: this should only be temporary until we have a better location for the module
+        'https://github.com/ohsu-computational-biology/ccc_client'
+    ]
+)
---
+++ test/importTest.py
@@ -0,0 +1,144 @@
+#!/usr/bin/env python
+
+#for debugging
+import sys, os
+import unittest
+import tempfile
+import csv
+import json
+import subprocess
+
+path = os.path.abspath(__file__)
+dir_path = os.path.dirname(path)
+dir_path = os.path.dirname(dir_path)
+sys.path.append(dir_path)
+
+from CCC import CCC_Client
+
+class TestCccClient(unittest.TestCase):
+
+    def test_integration_resource_registration(self):
+        print("testing single resource registration...")
+
+        sampleName = 'sample1'
+        self.registerSample(sampleName)
+
+        with tempfile.NamedTemporaryFile(mode='r', delete=False) as temp:
+            results = subprocess.check_output(['python', self.getScriptPath(), 'publish_resource',
+                  '--token=' + self.generateAuthToken(),
+                  '--filePath=' + temp.name,
+                  '--siteId=' + self.getSiteId(),
+                  '--user=' + self.getUser(),
+                  '--projectCode=' + self.getProject(),
+                  '--workflowId=workflow1',
+                  '--mimeType=mimeType',
+                  '--property=sample_id:' + sampleName,
+                  '-m'
+            ]).decode("utf-8")
+
+            results = json.loads(results)
+            self.assertEqual(results['projectCode'], self.getProject())
+            self.assertEqual(results['siteId'], self.getSiteId())
+            #we dont know what it will assign, but it should be a valid GUID
+            self.assertIsNotNone(results['ccc_did'])
+            self.assertEqual(len(results['ccc_did']), 36)
+            self.assertEqual(results['sampleId'], sampleName)
+            self.assertEqual(results['propToCopy'], 'NotNull')
+
+
+    def test_integration_copy_properties(self):
+        print("testing copying properties...")
+
+        sampleName = 'sample2'
+        self.registerSample(sampleName)
+
+        with tempfile.NamedTemporaryFile(mode='r', delete=False) as temp:
+            mockCCC_DID = 'mockCCC_DID'
+            results = subprocess.check_output(['python', self.getScriptPath(), 'publish_resource',
+                  '--token=' + self.generateAuthToken(),
+                  '--filePath=' + temp.name,
+                  '--siteId=' + self.getSiteId(),
+                  '--user=' + self.getUser(),
+                  '--projectCode=' + self.getProject(),
+                  '--workflowId=workflow1',
+                  '--mimeType=mime_type',
+                  '--property=sample_id:' + sampleName,
+                  '--property=ccc_did:' + mockCCC_DID,
+                  '--property=siteId:thisShouldNotCopy',
+                  '--property=projectCode:thisShouldNotCopy',
+                  '-m'
+            ]).decode("utf-8")
+
+            results = json.loads(results)
+
+            #these should match the projectCode argument, not the duplicate assigned through property=projectCode
+            self.assertEqual(results['projectCode'], self.getProject())
+            self.assertEqual(results['siteId'], self.getSiteId())
+
+            self.assertEqual(results['ccc_did'], mockCCC_DID)
+
+            #we expect these to be inherited from the sample
+            self.assertEqual(results['sampleId'], sampleName)
+            self.assertEqual(results['propToCopy'], 'NotNull')
+
+
+    def registerSample(self, sampleId):
+        with tempfile.NamedTemporaryFile(mode='w', delete=False) as tsv:
+            writer = csv.writer(tsv, delimiter='\t', lineterminator='\n')
+            writer.writerow(['sampleId', 'propToCopy'])
+            writer.writerow([sampleId, 'NotNull'])
+
+        results = subprocess.call(['python', self.getScriptPath(), 'publish_batch',
+              '--token=' + self.generateAuthToken(),
+              '--tsv=' + tsv.name,
+              '--siteId=' + self.getSiteId(),
+              '--user=' + self.getUser(),
+              '--projectCode=' + self.getProject(),
+              '--domain=sample'
+              ])
+
+        print('published sample: ' + sampleId)
+
+
+    def test_unit_index_names(self):
+        token = self.generateAuthToken()
+        ccc = CCC_Client(token)
+        rp = CCC_Client.RowParser(None, self.getSiteId(), self.getUser(), self.getProject(), 'resource', None, ccc.DomainDescriptors, token, True)
+
+        #index names
+        self.assertEqual(rp.getIndexNameForDomain('resource'), self.getProject().lower() + '-' + 'aggregated-resource')
+        self.assertEqual(rp.getIndexNameForDomain('sample'), self.getProject().lower() + '-' + 'sample')
+        self.assertEqual(rp.getIndexNameForDomain('specimen'), self.getProject().lower() + '-' + 'specimen')
+        self.assertEqual(rp.getIndexNameForDomain('individual'), self.getProject().lower() + '-' + 'individual')
+
+        #row keys
+        rowMap = {
+            'individual_id': 'PATIENT1',
+            'specimen_id': 'specImen1',
+            'sample_id': 'saMple1',
+            'ccc_did': 'ccc_did'
+        }
+        self.assertEqual(rp.generateKeyForDomain(rowMap, 'resource'), 'ccc_did')
+        self.assertEqual(rp.generateKeyForDomain(rowMap, 'sample'), self.getProject().lower() + '-' + 'sample' + '-sample1')
+        self.assertEqual(rp.generateKeyForDomain(rowMap, 'specimen'), self.getProject().lower() + '-' + 'specimen-specimen1')
+        self.assertEqual(rp.generateKeyForDomain(rowMap, 'individual'), self.getProject().lower() + '-' + 'individual-patient1')
+
+
+    def generateAuthToken(self):
+        #not yet implemented
+        return 'notYetImplemented'
+
+    def getScriptPath(self):
+        return os.path.join(dir_path, './bin/ccc_import')
+
+    def getSiteId(self):
+        return 'testSite'
+
+    def getProject(self):
+        return 'testProject'
+
+    def getUser(self):
+        return 'me'
+
+if __name__ == '__main__':
+    unittest.main()
